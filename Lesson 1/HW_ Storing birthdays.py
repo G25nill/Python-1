54ac73fd6d9cc7cc17a9dc46f8673b371d72d603
G25nill <snigdha.esh@gmail.com>
2026-03-22 00:16:56 +0600

    lesson1_HW(Storing birthdays)_done

---
+++ Lesson 1/HW_ Storing birthdays.py
@@ -0,0 +1,15 @@
+# Taking 5 birthday dates as input
+
+birthday1 = input("Enter birthday 1: ")
+birthday2 = input("Enter birthday 2: ")
+birthday3 = input("Enter birthday 3: ")
+birthday4 = input("Enter birthday 4: ")
+birthday5 = input("Enter birthday 5: ")
+
+# Displaying the Dates
+print("\nStored date of Birthdays1:")
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
