824e094a2ef77d725b0a67710d032b6b81b571ac
G25nill <snigdha.esh@gmail.com>
2026-03-22 00:25:05 +0600

    lesson1_HW(Storing birthdays)_done

--- Lesson 1/HW_ Storing birthdays.py
+++ Lesson 1/HW_ Storing birthdays.py
@@ -1,15 +1,31 @@
-# Taking 5 birthday dates as input
+# Taking inputs from friends:
 
-birthday1 = input("Enter birthday 1: ")
-birthday2 = input("Enter birthday 2: ")
-birthday3 = input("Enter birthday 3: ")
-birthday4 = input("Enter birthday 4: ")
-birthday5 = input("Enter birthday 5: ")
+print("Enter details of 5 friends:\n")
 
-# Displaying the Dates
-print("\nStored date of Birthdays:")
-print("Birthday 1:", birthday1)
-print("Birthday 2:", birthday2)
-print("Birthday 3:", birthday3)
-print("Birthday 4:", birthday4)
-print("Birthday 5:", birthday5)
+# Friend 1
+name1 = input("Enter name of friend 1: ")
+birthday1 = input("Enter birthday: ")
+
+# Friend 2
+name2 = input("\nEnter name of friend 2: ")
+birthday2 = input("Enter birthday: ")
+
+# Friend 3
+name3 = input("\nEnter name of friend 3: ")
+birthday3 = input("Enter birthday: ")
+
+# Friend 4
+name4 = input("\nEnter name of friend 4: ")
+birthday4 = input("Enter birthday: ")
+
+# Friend 5
+name5 = input("\nEnter name of friend 5: ")
+birthday5 = input("Enter birthday: ")
+
+# Displaying results
+print("\n--- Birthday Dates List ---")
+print(name1, "-", birthday1)
+print(name2, "-", birthday2)
+print(name3, "-", birthday3)
+print(name4, "-", birthday4)
+print(name5, "-", birthday5)
