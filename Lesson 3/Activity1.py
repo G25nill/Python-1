b513df444bf41421702105e814aba52de10a5c45
G25nill <snigdha.esh@gmail.com>
2026-03-28 10:21:22 +0600

    lesson3_Activity1_done

---
+++ Lesson 3/Activity1.py
@@ -0,0 +1,14 @@
+# Storing values
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+# FInding the total of trees
+sum= tree1+tree2+tree3+tree5+tree5
+print("the sum of all the 5 trees is:", sum)
+
+# Finding the average of trees
+average=sum/5
+print("the average of all the tree is :", average)
