333f264e11ecd0a04c1b5bb3c36164a1e3e80647
G25nill <snigdha.esh@gmail.com>
2026-07-31 12:44:46 +0600

    lesson19_activity1_done

---
+++ Lesson 19/activity1.py
@@ -0,0 +1,31 @@
+import math
+
+try:
+    a = float(input("Enter a number: "))
+    b = float(input("Enter another number: "))
+    c = input("Enter you action (multipy/divide/add/subtract): ")
+except ValueError:
+    print("That is not a number!. Please enter a new number")
+
+if c== add:
+    def add(a, b):
+        print(a + b)
+
+result = add(a, b)
+else:
+    def sub(a, b):
+        print(a - b)
+
+result = add(a, b)
+else:
+def multiply(a, b):
+    print(a * b)
+
+result = add(a, b)
+elif:
+def add(a, b):
+    print(a / b)
+
+result = add(a, b)
+
+
