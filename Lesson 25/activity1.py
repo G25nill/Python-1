351171bf6b2adbacbef34f8ad111de64ba89fad2
G25nill <snigdha.esh@gmail.com>
2026-08-28 12:47:08 +0600

    lesson25_activity1_.done

---
+++ Lesson 25/activity1.py
@@ -0,0 +1,20 @@
+student = {"name": "Alice", "score": 95 , "name": "Bob", "score": 87 , "name": "Max", "score": 88 , "name": "Alex", "score": 20 , "name": "Tom", "score": 99 ,}
+
+"""for key, value in student.pair():
+    print(key, ":", value)"""
+
+Search_Student= input("Enter a student name:")
+Pair = student.get("student", "Unknown")
+print(Pair)
+
+
+for i in student:
+    Avg_score= student.get("score")
+    print(student.score)
+
+
+"""for i in Pair(1, 6):
+    if pai
+print(squares)"""
+
+
