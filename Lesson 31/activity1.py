7cf3860af4cd1fa57b72294ddeabfc4b314dc6d3
G25nill <snigdha.esh@gmail.com>
2026-09-19 12:41:53 +0600

    Lesson31_activity1_done

---
+++ Lesson 31/activity1.py
@@ -0,0 +1,15 @@
+Book_Borrow= str("Name of the book you want to borrow: ")
+Book_Return= str("Name of the book you want to return: ")
+
+class Book:
+    def __init__(self, title, is_borrowed):
+        self.__is_borrowed = False
+
+    def Trueborrow(self):
+        is_borrowed = True
+        print("Availabe for borrowing:")
+
+Book1 = Book()
+for book in Book1:
+    animal.___())
+
